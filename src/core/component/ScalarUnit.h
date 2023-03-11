d51058017edaa37475fae7a39e1e82570f03d17f
Xyfuture <49277529+Xyfuture@users.noreply.github.com>
2023-03-11 15:02:53 +0800

    update

--- src/core/component/ScalarUnit.h
+++ src/core/component/ScalarUnit.h
@@ -7,8 +7,20 @@
 #ifndef CORE_COMPONENT_SCALARUNIT_H_
 #define CORE_COMPONENT_SCALARUNIT_H_
 
+#include <systemc>
+#include "config/Config.hpp"
 
-class ScalarUnit {
+
+using namespace sc_core;
+
+class ScalarUnit:public sc_module {
+    SC_HAS_PROCESS(ScalarUnit);
+public:
+    ScalarUnit(const sc_module_name& name,const CoreConfig& config);
+
+
+private:
+    CoreConfig& config;
 
 };
 
