42f16816b464d910bf84488ae91d649447057d23
Xyfuture <49277529+Xyfuture@users.noreply.github.com>
2023-03-13 00:10:23 +0800

    inst

--- src/isa/Instruction.h
+++ src/isa/Instruction.h
@@ -3,19 +3,35 @@
 //
 
 
-
+#pragma once
 #ifndef INST_INSTRUCTION_H_
 #define INST_INSTRUCTION_H_
 
+#include <memory>
 
-class Instruction {
-public:
-    Instruction();
-    ~Instruction();
+#include "isa/ISA.h"
+
+struct OffsetField{
+    int offset_value ;
+    int offset_select ;
+    // select function
+};
+
+
+struct VectorField{
+
+};
+
+
+struct Instruction {
+    // Common field
 
     int rs1_addr = 0;
     int rs2_addr = 0;
     int rd_addr = 0;
+
+    // vector field
+    std::unique_ptr<VectorField> vecl
 
 };
 
